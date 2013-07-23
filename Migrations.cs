6a5b297e93b15ac0fdb6134c667cac00b759424e
Nicholas Mayne <Jetski5822@hotmail.com>
2013-07-24 00:08:39 +0100

    Updatign to Orchard 1.7 and bumping version

--- Migrations.cs
+++ Migrations.cs
@@ -2,29 +2,35 @@
 using Orchard.ContentManagement.MetaData;
 using Orchard.Core.Contents.Extensions;
 using Orchard.Data.Migration;
+using Orchard.Localization;
 
 namespace NGM.ContentViewCounter {
     public class Migrations : DataMigrationImpl {
+        public Migrations() {
+            T = NullLocalizer.Instance;
+        }
+
+        public Localizer T { get; set; }
+
         public int Create() {
-            SchemaBuilder.CreateTable("UserViewCounterRecord",
-                table => table
-                    .Column<int>("Id", column => column.PrimaryKey().Identity())
-                    .Column<DateTime>("CreatedUtc")
-                    .Column<int>("ContentItemRecord_id")
-                    .Column<string>("ContentType")
-                    .Column<string>("Username")
-                    .Column<string>("Hostname")
-                );
+            ContentDefinitionManager.AlterPartDefinition("UserViewPart", builder => builder
+                .Attachable()
+                .WithDescription(T("Attaches a view counter to a content type. When a user view that item the counter is increased.").Text));
 
-            ContentDefinitionManager.AlterPartDefinition("UserViewPart", builder => builder.Attachable());
-
-            return 1;
+            return 3;
         }
 
         public int UpdateFrom1() {
             SchemaBuilder.DropTable("UserViewCounterRecord");
 
             return 2;
+        }
+
+        public int UpdateForm2() {
+            ContentDefinitionManager.AlterPartDefinition("UserViewPart", builder => builder
+                .WithDescription(T("Attaches a view counter to a content type. When a user view that item the counter is increased.").Text));
+
+            return 3;
         }
     }
 }
